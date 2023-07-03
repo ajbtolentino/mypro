f50e2e7826c0fd2eb892f9cee653fd9bdea3abf2
Allan Tolentino <ajbtolentino@gmail.com>
2023-07-03 23:50:15 +0800

    Added ocelot api gateway

--- Api/MyPro.Gateway.Api/Program.cs
+++ Api/MyPro.Gateway.Api/Program.cs
@@ -1,20 +1,14 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using IdentityServer4;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Microsoft.VisualBasic;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
-// accepts any access token issued by identity server
+//// accepts any access token issued by identity server
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
                 {
@@ -26,71 +20,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     };
                 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Gateway API", Version = "v1" });
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        BearerFormat = "Bearer",
-        Flows = new OpenApiOAuthFlows
-        {
-            AuthorizationCode = new OpenApiOAuthFlow
-            {
-                AuthorizationUrl = new Uri($"{builder.Configuration.GetValue<string>("AuthUrl")}/connect/authorize"),
-                TokenUrl = new Uri($"{builder.Configuration.GetValue<string>("AuthUrl")}/connect/token"),
-            }
-        },
-        Type = SecuritySchemeType.OAuth2,
-        OpenIdConnectUrl = new Uri($"{builder.Configuration.GetValue<string>("AuthUrl")}/.well-known/openid-configuration")
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id=JwtBearerDefaults.AuthenticationScheme,
-                },
-                Scheme = "oauth2",
-                Name = JwtBearerDefaults.AuthenticationScheme,
-                In = ParameterLocation.Header
-            },
-            new string[]{}
-        }
-    });
-});
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseSwagger();
-
-app.UseSwaggerUI(options =>
-{
-    options.OAuthClientId("public-api");
-    options.OAuthScopes("profile", "openid", "read", "email" );
-    options.OAuthUsePkce();
-    options.OAuth2RedirectUrl("https://localhost:6001/swagger/oauth2-redirect.html");
-});
+app.UseOcelot().Wait();
 
 app.Run();
 
--- Api/MyPro.Shopping.Api/Controllers/IntegrationController.cs
+++ Api/MyPro.Shopping.Api/Controllers/IntegrationController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Net.Http.Headers;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace MyPro.Shopping.Api.Controllers;
 
@@ -8,16 +12,28 @@ namespace MyPro.Shopping.Api.Controllers;
 [Authorize]
 public class IntegrationController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
     private readonly ILogger<IntegrationController> _logger;
 
-    public IntegrationController(ILogger<IntegrationController> logger)
+    public IntegrationController(ILogger<IntegrationController> logger, IConfiguration configuration)
     {
+        _configuration = configuration;
         _logger = logger;
     }
 
     [HttpGet(nameof(TestConnection))]
-    public IActionResult TestConnection()
+    public async Task<IActionResult> TestConnection()
     {
+        var client = new HttpClient();
+        var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+        var disco = await client.GetDiscoveryDocumentAsync($"{_configuration.GetValue<string>("AuthUrl")}/.well-known/openid-configuration");
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var response = await client.GetUserInfoAsync(new UserInfoRequest
+        {
+            Address = disco.UserInfoEndpoint,
+            Token = accessToken
+        });
+
         return new JsonResult("success");
     }
 }
